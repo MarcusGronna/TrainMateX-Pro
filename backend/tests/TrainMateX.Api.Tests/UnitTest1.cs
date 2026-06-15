eef98c6d19732e4372d6b65fd51ad15a50012749
Marcus Grönnå <mgronna@gmail.com>
2026-06-15 20:39:00 +0200

    feat(api): create new .net solution and first scaffold of web api

---
+++ backend/tests/TrainMateX.Api.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace TrainMateX.Api.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ docs/decisions/0001-use-monorepo.md
@@ -0,0 +1,8 @@
+# Decision to use monorepo
+
+## TrainmateX - Pro
+
+```text
+Table | To | Test
+
+text```
