5bd1af7ae444de23621c88d8335c505340a2adff
Marcus Grönnå <mgronna@gmail.com>
2026-06-19 09:16:03 +0200

    feat(api): add /exercises endpoint and Exercise model
    Replaced default endpoint with /exercises to return exercise data. Introduced Exercise class with properties and a static method to load exercises from a JSON file. Added System.Text.Json for serialization support.

--- backend/src/TrainMateX.Api/Program.cs
+++ backend/src/TrainMateX.Api/Program.cs
@@ -1,6 +1,29 @@
+using System.Text.Json;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/exercises", () => Exercise.GetExercises());
+
 
 app.Run();
+
+
+public class Exercise
+{
+    public string Id { get; set; } = String.Empty;
+    public string Name { get; set; } = String.Empty;
+    public string MuscleGroup { get; set; } = String.Empty;
+    public string DifficultyLevel { get; set; } = String.Empty;
+
+    public static List<Exercise> GetExercises()
+    {
+        var json = File.ReadAllText("exercise-list-data.json");
+        var data = JsonSerializer.Deserialize<Dictionary<string, Exercise>>(json, new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true
+        });
+
+        return data?.Values.ToList() ?? [];
+    }
+}
