c6d0bcc2e792b08265992e8be92f823528d66745
Marcus Grönnå <mgronna@gmail.com>
2026-06-25 14:44:53 +0200

    feat(web): add ExerciseList component to Home page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,10 +1,11 @@
-import Image from "next/image";
+import ExerciseList from "@/features/exercises/components/ExerciseList";
 
 export default function Home() {
   return (
     <>
       <main className="text-center">
         <h1 className="">TrainMateX - PRO</h1>
+        <ExerciseList />
       </main>
     </>
   );
