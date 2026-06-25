1ece2eb3ddac5dca273ae37113ba38b4ec597dd1
Marcus Grönnå <mgronna@gmail.com>
2026-06-25 14:53:36 +0200

    fix(web): update API endpoint and wrap exercise items in button for better interaction

--- frontend/src/features/exercises/components/ExerciseList.tsx
+++ frontend/src/features/exercises/components/ExerciseList.tsx
@@ -17,7 +17,7 @@ export default function ExerciseList() {
   useEffect(() => {
     const loadExercises = async () => {
       try {
-        const baseUrl: string = "https://localhost:7087/api/exercises";
+        const baseUrl: string = "http://localhost:5193/api/exercises";
         const response = await fetch(baseUrl);
 
         if (!response.ok) {
@@ -39,20 +39,19 @@ export default function ExerciseList() {
       <div className="text-sm text-gray-500">{exercises.length} exercises loaded</div>
       <ul className="grid gap-3 sm:grid-cols-2 lg:grid-cols-3">
         {exercises.map((exercise) => (
-          <li
-            key={exercise.id}
-            className="rounded-xl border border-gray-200 bg-blend-darken p-4 shadow-sm transition hover:shadow-md"
-          >
-            <h3>{exercise.name}</h3>
-            <div className="mt-3 flex flex-wrap gap-2 text-xs">
-              <span className="rounded-full bg-blue-100 px-2.5 py-1 font-medium text-blue-700">
-                {exercise.muscleGroup}
-              </span>
-              <span className="rounded-full bg-purple-100 px-2.5 py-1 font-medium text-purple-700">
-                {exercise.difficultyLevel}
-              </span>
-            </div>
-          </li>
+          <button key={exercise.id} className="cursor-pointer">
+            <li className="rounded-xl border border-gray-200 bg-blend-darken p-4 shadow-sm transition hover:shadow-md hover:bg-fuchsia-950">
+              <h3>{exercise.name}</h3>
+              <div className="mt-3 flex flex-wrap gap-2 text-xs">
+                <span className="rounded-full bg-blue-100 px-2.5 py-1 font-medium text-blue-700">
+                  {exercise.muscleGroup}
+                </span>
+                <span className="rounded-full bg-purple-100 px-2.5 py-1 font-medium text-purple-700">
+                  {exercise.difficultyLevel}
+                </span>
+              </div>
+            </li>
+          </button>
         ))}
       </ul>
     </section>
