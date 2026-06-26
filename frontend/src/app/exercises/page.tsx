6038cf1b5dd7a620802431007a27deca6c2018ea
Marcus Grönnå <mgronna@gmail.com>
2026-06-26 15:13:15 +0200

    feat(web): create ExerciseDetailsPage and refactor Home component to link to Exercise Library

---
+++ frontend/src/app/exercises/page.tsx
@@ -0,0 +1,10 @@
+import { ExerciseList } from "@/features/exercises/components/ExerciseList";
+
+export default function ExercisesPage() {
+  return (
+    <main>
+      <h1>ExerciseLibrary</h1>
+      <ExerciseList />
+    </main>
+  );
+}
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,12 +1,15 @@
-import ExerciseList from "@/features/exercises/components/ExerciseList";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <>
-      <main className="text-center">
-        <h1 className="">TrainMateX - PRO</h1>
-        <ExerciseList />
-      </main>
-    </>
+    <main className="text-center">
+      <h1 className="">TrainMateX PRO</h1>
+      <Link
+        href="/exercises"
+        className="rounded-full bg-purple-100 px-2.5 py-1 font-medium text-purple-700 hover:bg-purple-600 hover:text-purple-100"
+      >
+        Exercise Library
+      </Link>
+    </main>
   );
 }
